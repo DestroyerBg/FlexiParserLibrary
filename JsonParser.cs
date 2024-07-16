351c0ce56c6375dd060bcf831454485d66a21bfc
DestroyerBg <gabriel-kulev@abv.bg>
2024-07-16 12:57:13 +0300

    Initial commit

---
+++ JsonParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace FlexiParser
+{
+    public static class JsonParser
+    {
+        public static T ParseJson<T>(string inputJson)
+        {
+            var result = JsonConvert.DeserializeObject<T>(inputJson);
+            return result;
+        }
+
+        public static string GetJson(Object collection, bool isCamelCase, bool isOmitNullableValues = false)
+        {
+            var jsonOptions = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = isOmitNullableValues == true ? NullValueHandling.Ignore : NullValueHandling.Include,
+                ContractResolver = isCamelCase == true ? new CamelCasePropertyNamesContractResolver() : null,
+            };
+            var result = JsonConvert.SerializeObject(collection, jsonOptions);
+            return result;
+        }
+    }
+}
---
+++ XmlParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace FlexiParser
+{
+    public static class XmlParser
+    {
+        public static string SerializeToXml<T>(T dto, string xmlRootAttribute, bool omitDeclaration = false)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttribute));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = omitDeclaration,
+                Encoding = new UTF8Encoding(false),
+                Indent = true
+            };
+
+            using (StringWriter stringWriter = new StringWriter(stringBuilder, CultureInfo.InvariantCulture))
+            using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, settings))
+            {
+                XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+                xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+                try
+                {
+                    xmlSerializer.Serialize(xmlWriter, dto, xmlSerializerNamespaces);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static T DeserializeXML<T>(string inputXml, string xmlRootAttribute)
+        {
+            var serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttribute));
+            using var reader = new StringReader(inputXml);
+            return (T)serializer.Deserialize(reader);
+        }
+    }
+}
